ed997fb2ffc0823241f585163b3c9cd8365c4590
spwashi <sam@spwashi.com>
2024-05-16 13:30:16 -0500

    [write] The Adventures of Hemera Nyx in the Galaxy of the Future

---
+++ tahm_gotf/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let seriesTitle = "The Adventures of Hemera Nyx";
+    let bookTitle   = "The Galaxy of The Future";
+
+    println!("{} {}", seriesTitle, bookTitle);
+    println!("=[RSK]=");
+}
