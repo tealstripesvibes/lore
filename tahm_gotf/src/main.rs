3c19333dd1ed3380b949bb9ff601762169ac9a25
spwashi <sam@spwashi.com>
2024-05-16 21:38:49 -0500

    [refactor]<flow>

--- tahm_gotf/src/main.rs
+++ tahm_gotf/src/main.rs
@@ -1,70 +1,31 @@
 mod ui;
 mod pages;
 
-fn page_1() {
-    pages::page_001::page();
-}
-
-fn page_2() {
-    pages::page_002::page();
-}
-
-fn page_3() {
-    pages::page_003::page();
-}
-
-fn page_4() {
-    pages::page_004::page();
-}
-
-fn page_5() {
-    pages::page_005::page();
-}
-
-fn page_6() {
-    pages::page_006::page();
-}
-
-fn page_7() {
-    pages::page_007::page();
-}
-
-fn page_8() {
-    pages::page_008::page();
-}
-
-fn page_9() {
-    pages::page_009::page();
-}
-
-fn page_10() {
-    pages::page_010::page();
-}
-
-fn page_11() {
-    pages::page_011::page();
-}
-
-fn page_12() {
-    pages::page_012::page();
-}
-
-fn page_13() {
-    pages::page_013::page();
-}
+use std::io;
 
 fn main() {
-    page_1();
-    page_2();
-    page_3();
-    page_4();
-    page_5();
-    page_6();
-    page_7();
-    page_8();
-    page_9();
-    page_10();
-    page_11();
-    page_12();
-    page_13();
+    println!("Please enter starter page");
+
+    let mut input = String::new();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+
+    let page_number: u32 = input.trim().parse().expect("Could not parse page number");
+
+    let mut i = 0;
+    i += 1; if i >= page_number { pages::page_001::page(); }
+    i += 1; if i >= page_number { pages::page_002::page(); }
+    i += 1; if i >= page_number { pages::page_003::page(); }
+    i += 1; if i >= page_number { pages::page_004::page(); }
+    i += 1; if i >= page_number { pages::page_005::page(); }
+    i += 1; if i >= page_number { pages::page_006::page(); }
+    i += 1; if i >= page_number { pages::page_007::page(); }
+    i += 1; if i >= page_number { pages::page_008::page(); }
+    i += 1; if i >= page_number { pages::page_009::page(); }
+    i += 1; if i >= page_number { pages::page_010::page(); }
+    i += 1; if i >= page_number { pages::page_011::page(); }
+    i += 1; if i >= page_number { pages::page_012::page(); }
+    i += 1; if i >= page_number { pages::page_013::page(); }
 }
