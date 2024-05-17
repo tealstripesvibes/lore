6bb1422e4aad17ea1fce4ebe59b50a267cac68ca
spwashi <sam@spwashi.com>
2024-05-16 20:10:00 -0500

    [allocating]

--- tahm_gotf/src/main.rs
+++ tahm_gotf/src/main.rs
@@ -9,7 +9,62 @@ fn page_2() {
     pages::page_002::page();
 }
 
+fn page_3() {
+
+}
+
+fn page_4() {
+
+}
+
+fn page_5() {
+
+}
+
+fn page_6() {
+
+}
+
+fn page_7() {
+
+}
+
+fn page_8() {
+
+}
+
+fn page_9() {
+
+}
+
+fn page_10() {
+
+}
+
+fn page_11() {
+
+}
+
+fn page_12() {
+
+}
+
+fn page_13() {
+
+}
+
 fn main() {
     page_1();
     page_2();
+    page_3();
+    page_4();
+    page_5();
+    page_6();
+    page_7();
+    page_8();
+    page_9();
+    page_10();
+    page_11();
+    page_12();
+    page_13();
 }
