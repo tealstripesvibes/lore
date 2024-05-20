2dae03468f08d7b911ca1b02fe3c661683d7635e
spwashi <sam@spwashi.com>
2024-05-20 02:14:38 -0500

    [refactor]<enum,extraction>

--- tahm_gotf/src/pages/page_002.rs
+++ tahm_gotf/src/pages/page_002.rs
@@ -1,11 +1,50 @@
 use crate::ui;
 
 pub fn section_header(text: &str) -> String {
-    return format!("======\n{}\n======", text)
+    return format!("======\n{}\n======", text);
+}
+
+#[derive(Debug)]
+enum PotentialTrigger {
+    AbusiveRelationship(),
+    Alcohol(),
+    Assault(),
+    AttemptedMurder(),
+    Blood(),
+    Bullying(),
+    Death(),
+    Depression(),
+    Drugs(),
+    EmotionalAbuse(),
+    Eugenics(),
+    Fire(),
+    Gore(),
+    GunViolence(),
+    Hallucinations(),
+    Murder(),
+    PhysicalAbuse(),
+    Profanity(),
+    PTSD(),
+    Skeletons(),
+    Spiders(),
+    Violence(),
+}
+
+impl std::fmt::Display for PotentialTrigger {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            PotentialTrigger::AbusiveRelationship() => write!(f, "{}", "Abusive Relationship"),
+            PotentialTrigger::AttemptedMurder() => write!(f, "{}", "Attempted Murder"),
+            PotentialTrigger::EmotionalAbuse() => write!(f, "{}", "Emotional Abuse"),
+            PotentialTrigger::GunViolence() => write!(f, "{}", "Gun Violence"),
+            PotentialTrigger::PhysicalAbuse() => write!(f, "{}", "Physical Abuse"),
+            _ => write!(f, "{:?}", self),
+        }
+    }
 }
 
 pub fn page() {
-    let license = 
+    let license =
         "All rights reserved.\n \
          This book \n \
          or any portion thereof \n \
@@ -20,33 +59,36 @@ pub fn page() {
     ui::pause(true);
 
     let trigger_warnings = [
-        "Abusive Relationship", 
-        "Alcohol", 
-        "Assault",
-        "Attempted Murder",
-        "Blood",
-        "Bullying",
-        "Death",
-        "Depression",
-        "Drugs",
-        "Emotional Abuse",
-        "Eugenics",
-        "Fire",
-        "Gore",
-        "Gun Violence",
-        "Hallucinations",
-        "Murder",
-        "Physical Abuse",
-        "Profanity",
-        "PTSD",
-        "Skeletons",
-        "Spiders",
-        "Violence",
+        PotentialTrigger::AbusiveRelationship(),
+        PotentialTrigger::Alcohol(),
+        PotentialTrigger::Assault(),
+        PotentialTrigger::AttemptedMurder(),
+        PotentialTrigger::Blood(),
+        PotentialTrigger::Bullying(),
+        PotentialTrigger::Death(),
+        PotentialTrigger::Depression(),
+        PotentialTrigger::Drugs(),
+        PotentialTrigger::EmotionalAbuse(),
+        PotentialTrigger::Eugenics(),
+        PotentialTrigger::Fire(),
+        PotentialTrigger::Gore(),
+        PotentialTrigger::GunViolence(),
+        PotentialTrigger::Hallucinations(),
+        PotentialTrigger::Murder(),
+        PotentialTrigger::PhysicalAbuse(),
+        PotentialTrigger::Profanity(),
+        PotentialTrigger::PTSD(),
+        PotentialTrigger::Skeletons(),
+        PotentialTrigger::Spiders(),
+        PotentialTrigger::Violence(),
     ];
-    let bug_report_memo  = "(Please contact RSK at RSK.Author@gmail.com if there are any missing warnings)";
+
+    let bug_report_memo = "(Please contact RSK at RSK.Author@gmail.com if there are any missing warnings)";
     let trigger_warnings = format!(
-        "Trigger Warnings:\n{}\n{}", 
-        trigger_warnings.join("\n"), 
+        "Trigger Warnings:\n{}\n{}",
+        trigger_warnings
+            .map(|potential_trigger: PotentialTrigger| potential_trigger.to_string())
+            .join("\n"),
         bug_report_memo
     );
 
